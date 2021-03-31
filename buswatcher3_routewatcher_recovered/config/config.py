7d04dec57aae29c35545db934d10abe8df2e8dff
Anthony Townsend <anthony@starcitygorup.us>
2021-03-31 17:26:47 -0400

    first commit including some recovered files from previous version

---
+++ buswatcher3_routewatcher_recovered/config/config.py
@@ -0,0 +1,12 @@
+# future the dict values can be used for other metadata later, another dict of route data copied over from old route_descriptions.json
+
+routes = {
+    '84':['a','b'],
+    '85':['a','b'],
+    '86':['a','b'],
+    '87':['a','b'],
+    '119':['a','b'],
+    '126':['a','b']
+    }
+
+source = 'nj'
