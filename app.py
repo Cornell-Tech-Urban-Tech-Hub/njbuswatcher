9186370c01e434f9a71a605aaf8e526bc52f3475
Anthony Townsend <anthony@starcitygroup.us>
2022-01-29 14:18:29 -0500

    removed heatmap

--- app.py
+++ app.py
@@ -135,165 +135,176 @@ with create_engine(db.get_db_url(*db.get_db_args(args,config))).connect() as con
     ################################################################################################################################################
     # all time by day and month -- calendar heatmap
     ################################################################################################################################################
+    # https://github.com/andodet/git-overview
+
+    heatmap_data = dash_data['observations_by_date']
+
+
+
+    ################################################################################################################################################
+    # all time by day and month -- calendar heatmap
+    ################################################################################################################################################
     # https://gist.github.com/bendichter/d7dccacf55c7d95aec05c6e7bcf4e66e
 
     # MIT LICENSE
 
-    st.subheader("History")
 
-    from plotly import subplots
-    import datetime
-    import plotly.graph_objs as go
-
-    def display_year(z,
-                     year: int = None,
-                     month_lines: bool = True,
-                     fig=None,
-                     row: int = None):
-
-        if year is None:
-            year = datetime.datetime.now().year
-
-        data = np.ones(365) * np.nan
-        data[:len(z)] = z
-
-
-        d1 = datetime.date(year, 1, 1)
-        d2 = datetime.date(year, 12, 31)
-
-        delta = d2 - d1
-
-        month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        month_days =   [31,    28,    31,     30,    31,     30,    31,    31,    30,    31,    30,    31]
-        month_positions = (np.cumsum(month_days) - 15)/7
-
-        dates_in_year = [d1 + datetime.timedelta(i) for i in range(delta.days+1)] #gives me a list with datetimes for each day a year
-        weekdays_in_year = [i.weekday() for i in dates_in_year] #gives [0,1,2,3,4,5,6,0,1,2,3,4,5,6,…] (ticktext in xaxis dict translates this to weekdays
-
-        weeknumber_of_dates = [int(i.strftime("%V")) if not (int(i.strftime("%V")) == 1 and i.month == 12) else 53
-                               for i in dates_in_year] #gives [1,1,1,1,1,1,1,2,2,2,2,2,2,2,…] name is self-explanatory
-        text = [str(i) for i in dates_in_year] #gives something like list of strings like ‘2018-01-25’ for each date. Used in data trace to make good hovertext.
-        #4cc417 green #347c17 dark green
-        colorscale=[[False, '#eeeeee'], [True, '#76cf63']]
-
-        # handle end of year
-
-
-        data = [
-            go.Heatmap(
-                x=weeknumber_of_dates,
-                y=weekdays_in_year,
-                z=data,
-                text=text,
-                hoverinfo='text',
-                xgap=3, # this
-                ygap=3, # and this is used to make the grid-like apperance
-                showscale=False,
-                colorscale=colorscale
-            )
-        ]
-
-
-        if month_lines:
-            kwargs = dict(
-                mode='lines',
-                line=dict(
-                    color='#9e9e9e',
-                    width=1
-                ),
-                hoverinfo='skip'
-
-            )
-            for date, dow, wkn in zip(dates_in_year,
-                                      weekdays_in_year,
-                                      weeknumber_of_dates):
-                if date.day == 1:
-                    data += [
-                        go.Scatter(
-                            x=[wkn-.5, wkn-.5],
-                            y=[dow-.5, 6.5],
-                            **kwargs
-                        )
-                    ]
-                    if dow:
-                        data += [
-                            go.Scatter(
-                                x=[wkn-.5, wkn+.5],
-                                y=[dow-.5, dow - .5],
-                                **kwargs
-                            ),
-                            go.Scatter(
-                                x=[wkn+.5, wkn+.5],
-                                y=[dow-.5, -.5],
-                                **kwargs
-                            )
-                        ]
-
-
-        layout = go.Layout(
-            # title='Daily Observations—history',
-            height=250,
-            yaxis=dict(
-                showline=False, showgrid=False, zeroline=False,
-                tickmode='array',
-                ticktext=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-                tickvals=[0, 1, 2, 3, 4, 5, 6],
-                autorange="reversed"
-            ),
-            xaxis=dict(
-                showline=False, showgrid=False, zeroline=False,
-                tickmode='array',
-                ticktext=month_names,
-                tickvals=month_positions
-            ),
-            font={'size':10, 'color':'#9e9e9e'},
-            plot_bgcolor=('#fff'),
-            margin = dict(t=40),
-            showlegend=False
-        )
-
-        if fig is None:
-            fig = go.Figure(data=data, layout=layout)
-        else:
-            fig.add_traces(data, rows=[(row+1)]*len(data), cols=[1]*len(data))
-            fig.update_layout(layout)
-            fig.update_xaxes(layout['xaxis'])
-            fig.update_yaxes(layout['yaxis'])
-
-
-        return fig
-
-
-    def display_years(z, years):
-        st.write(z,years)
-        fig = subplots.make_subplots(rows=len(years), cols=1, subplot_titles=years)
-        for i, year in enumerate(years):
-            data = z[i*365 : (i+1)*365]
-            display_year(data, year=year, fig=fig, row=i)
-            fig.update_layout(height=250*len(years))
-        return fig
-
-
-    def generate_data():
-        heatmap_data = dash_data['observations_by_date']
-        format = '%Y-%m-%d'
-        start = dt.datetime.strptime(heatmap_data[0]['date'], format)
-        num_dates = len(heatmap_data)
-        dates = [start + dt.timedelta(days=x) for x in range(0, num_dates)]
-        data = [d['num'] for d in heatmap_data]
-        # return dates, data
-        return (start.year, dates[-1].year), data
-
-    years, data = generate_data()
-    if len(years) == 1:
-        years = (years[0],)
-    elif len(years) > 1:
-        span = int(years[-1]) == int(years[0])
-        if span == 1:
-            years = (years[0],)
-        elif span > 1:
-            years = ([y for y in years])
-    st.plotly_chart(display_years(data, years))
+    #
+    # st.subheader("History")
+    #
+    # from plotly import subplots
+    # import datetime
+    # import plotly.graph_objs as go
+    #
+    # def display_year(z,
+    #                  year: int = None,
+    #                  month_lines: bool = True,
+    #                  fig=None,
+    #                  row: int = None):
+    #
+    #     if year is None:
+    #         year = datetime.datetime.now().year
+    #
+    #     data = np.ones(365) * np.nan
+    #     data[:len(z)] = z
+    #
+    #
+    #     d1 = datetime.date(year, 1, 1)
+    #     d2 = datetime.date(year, 12, 31)
+    #
+    #     delta = d2 - d1
+    #
+    #     month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    #     month_days =   [31,    28,    31,     30,    31,     30,    31,    31,    30,    31,    30,    31]
+    #     month_positions = (np.cumsum(month_days) - 15)/7
+    #
+    #     dates_in_year = [d1 + datetime.timedelta(i) for i in range(delta.days+1)] #gives me a list with datetimes for each day a year
+    #     weekdays_in_year = [i.weekday() for i in dates_in_year] #gives [0,1,2,3,4,5,6,0,1,2,3,4,5,6,…] (ticktext in xaxis dict translates this to weekdays
+    #
+    #     weeknumber_of_dates = [int(i.strftime("%V")) if not (int(i.strftime("%V")) == 1 and i.month == 12) else 53
+    #                            for i in dates_in_year] #gives [1,1,1,1,1,1,1,2,2,2,2,2,2,2,…] name is self-explanatory
+    #     text = [str(i) for i in dates_in_year] #gives something like list of strings like ‘2018-01-25’ for each date. Used in data trace to make good hovertext.
+    #     #4cc417 green #347c17 dark green
+    #     colorscale=[[False, '#eeeeee'], [True, '#76cf63']]
+    #
+    #     # handle end of year
+    #
+    #
+    #     data = [
+    #         go.Heatmap(
+    #             x=weeknumber_of_dates,
+    #             y=weekdays_in_year,
+    #             z=data,
+    #             text=text,
+    #             hoverinfo='text',
+    #             xgap=3, # this
+    #             ygap=3, # and this is used to make the grid-like apperance
+    #             showscale=False,
+    #             colorscale=colorscale
+    #         )
+    #     ]
+    #
+    #
+    #     if month_lines:
+    #         kwargs = dict(
+    #             mode='lines',
+    #             line=dict(
+    #                 color='#9e9e9e',
+    #                 width=1
+    #             ),
+    #             hoverinfo='skip'
+    #
+    #         )
+    #         for date, dow, wkn in zip(dates_in_year,
+    #                                   weekdays_in_year,
+    #                                   weeknumber_of_dates):
+    #             if date.day == 1:
+    #                 data += [
+    #                     go.Scatter(
+    #                         x=[wkn-.5, wkn-.5],
+    #                         y=[dow-.5, 6.5],
+    #                         **kwargs
+    #                     )
+    #                 ]
+    #                 if dow:
+    #                     data += [
+    #                         go.Scatter(
+    #                             x=[wkn-.5, wkn+.5],
+    #                             y=[dow-.5, dow - .5],
+    #                             **kwargs
+    #                         ),
+    #                         go.Scatter(
+    #                             x=[wkn+.5, wkn+.5],
+    #                             y=[dow-.5, -.5],
+    #                             **kwargs
+    #                         )
+    #                     ]
+    #
+    #
+    #     layout = go.Layout(
+    #         # title='Daily Observations—history',
+    #         height=250,
+    #         yaxis=dict(
+    #             showline=False, showgrid=False, zeroline=False,
+    #             tickmode='array',
+    #             ticktext=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    #             tickvals=[0, 1, 2, 3, 4, 5, 6],
+    #             autorange="reversed"
+    #         ),
+    #         xaxis=dict(
+    #             showline=False, showgrid=False, zeroline=False,
+    #             tickmode='array',
+    #             ticktext=month_names,
+    #             tickvals=month_positions
+    #         ),
+    #         font={'size':10, 'color':'#9e9e9e'},
+    #         plot_bgcolor=('#fff'),
+    #         margin = dict(t=40),
+    #         showlegend=False
+    #     )
+    #
+    #     if fig is None:
+    #         fig = go.Figure(data=data, layout=layout)
+    #     else:
+    #         fig.add_traces(data, rows=[(row+1)]*len(data), cols=[1]*len(data))
+    #         fig.update_layout(layout)
+    #         fig.update_xaxes(layout['xaxis'])
+    #         fig.update_yaxes(layout['yaxis'])
+    #
+    #
+    #     return fig
+    #
+    #
+    # def display_years(z, years):
+    #     st.write(z,years)
+    #     fig = subplots.make_subplots(rows=len(years), cols=1, subplot_titles=years)
+    #     for i, year in enumerate(years):
+    #         data = z[i*365 : (i+1)*365]
+    #         display_year(data, year=year, fig=fig, row=i)
+    #         fig.update_layout(height=250*len(years))
+    #     return fig
+    #
+    #
+    # def generate_data():
+    #     heatmap_data = dash_data['observations_by_date']
+    #     format = '%Y-%m-%d'
+    #     start = dt.datetime.strptime(heatmap_data[0]['date'], format)
+    #     num_dates = len(heatmap_data)
+    #     dates = [start + dt.timedelta(days=x) for x in range(0, num_dates)]
+    #     data = [d['num'] for d in heatmap_data]
+    #     # return dates, data
+    #     return (start.year, dates[-1].year), data
+    #
+    # years, data = generate_data()
+    # if len(years) == 1:
+    #     years = (years[0],)
+    # elif len(years) > 1:
+    #     span = int(years[-1]) == int(years[0])
+    #     if span == 1:
+    #         years = (years[0],)
+    #     elif span > 1:
+    #         years = ([y for y in years])
+    # st.plotly_chart(display_years(data, years))
 
 
 
